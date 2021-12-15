8794e6bc3e49e8b0414f0cabac52ce08a2a41d69
Dmitry Bimatov <dbimatov@gmail.com>
2021-12-14 17:27:59 -0800

    Add basic handlers to access schedules

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import flask
 import os
 
+import schedules
+
+
 # create a new flask application
 app = flask.Flask(__name__)
+
+# initialization: load all schedules to dictionary
+data_directory = os.path.join(app.root_path, 'data')
+schedules.all_schedules = schedules.loadAllSchedules(data_directory)
 
 
 @app.route("/")
@@ -14,3 +21,56 @@ def handler_main():
 def handler_health():
     response = flask.make_response("Health status: OK")
     return response, 200
+
+
+@app.route("/schedule", methods=['GET'])
+def handler_allSchedules():
+    ids = schedules.findAllSchedules()
+
+    response = {"schedules": ids}
+    return response
+
+
+@app.route("/schedule/<id>", methods=['GET'])
+def handler_getSchedule(id):
+    if id not in schedules.all_schedules:
+        flask.abort(404)
+        return
+
+    mode = flask.request.args.get("mode", "json")
+    mode = mode.lower()
+
+    response = None
+    if mode == "json":
+        response = flask.make_response(schedules.getJsonSchedule(id), 200)
+        response.mimetype = "application/json"
+    elif mode == "mwt":
+        response = flask.make_response(schedules.getMwtSchedule(id), 200)
+        response.mimetype = "text/plain"
+    elif mode == "log":
+        response = flask.make_response(schedules.getLogSchedule(id), 200)
+        response.mimetype = "text/plain"
+    else:
+        flask.abort(
+            400, "Invalid mode value. Valid values are: json | mwt | log.")
+    return response
+
+
+@app.route("/find")
+def handler_find():
+    numPlayers = flask.request.args.get("players", 0)
+    numTables = flask.request.args.get("tables", 0)
+    numAttempts = flask.request.args.get("distance", 0)
+
+    try:
+        configuration = {
+            "numPlayers": int(numPlayers),
+            "numTables": int(numTables),
+            "numAttempts": int(numAttempts)
+        }
+    except ValueError:
+        flask.abort(400)
+
+    ids = schedules.findSchedules(configuration)
+    response = {"schedules": ids}
+    return response
---
+++ schedules.py
@@ -0,0 +1,62 @@
+import os
+import json
+
+
+'''
+Global dictionary with all schedules
+'''
+all_schedules = {}
+
+
+def loadAllSchedules(data_directory):
+    '''
+    Load all schedules from .txt files in specified directory
+    '''
+    print(f"Loading schedules from: {data_directory}")
+    result = {}
+    for root, dirs, files in os.walk(data_directory):
+        for filename in files:
+            name, ext = os.path.splitext(filename)
+            if ext != ".txt":
+                print(f"Skipping file: {filename}")
+                continue
+
+            full_filename = os.path.join(root, filename)
+            print(f"Loading from file: {full_filename}")
+            with open(full_filename) as f:
+                s = json.load(f)
+                result[name] = s
+    print(f"Total number of schedules loaded: {len(result)}")
+    return result
+
+
+def findAllSchedules() -> list[str]:
+    result = [id for id in all_schedules.keys()]
+    return result
+
+
+def findSchedules(config: dict) -> list[str]:
+    result = []
+    for id, item in all_schedules.items():
+        curr_config = item["configuration"]
+        if config["numTables"] and config["numTables"] != curr_config["numTables"]:
+            continue
+        if config["numPlayers"] and config["numPlayers"] != curr_config["numPlayers"]:
+            continue
+        if config["numAttempts"] and config["numAttempts"] != curr_config["numAttempts"]:
+            continue
+        result.append(id)
+
+    return result
+
+
+def getJsonSchedule(id) -> dict:
+    return all_schedules[id]
+
+
+def getMwtSchedule(id) -> str:
+    return "Not implemented"
+
+
+def getLogSchedule(id) -> str:
+    return "Not implemented"
